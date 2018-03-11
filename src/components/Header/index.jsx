a9994ce74c92b5a6d5244d38a25b7ba6fe412f91
Todd Matthews <gtmatthews@gmail.com>
2018-03-11 14:51:57 -0400

    Initial commmit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,109 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Link from 'gatsby-link';
+import styled from 'styled-components';
+import Img from 'gatsby-image';
+
+const HeaderWrapper = styled.div`
+  background: #524763;
+  margin-bottom: 1.45rem;
+  overflow: hidden;
+  position: relative;
+  height: ${({ isHome }) => (isHome ? '70vh' : '20vh')};
+`;
+
+const HeaderContainer = styled.div`
+  margin: 0 auto;
+  maxwidth: 960px;
+  padding: 1.45rem 1.0875rem;
+  position: relative;
+  z-index: 2;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const MainNav = styled.nav`
+  ul {
+    list-style: none;
+    display: flex;
+  }
+  li {
+    margin-left: 1rem;
+    font-family: 'Helvetica Neue';
+    a {
+      text-decoration: none;
+      color: white;
+      &:hover {
+        border-bottom: 3px solid #524763;
+      }
+    }
+  }
+`;
+
+export default class Header extends Component {
+  componentDidUpdate = (prevProps, prevState) => {
+    const { location } = this.props;
+    if (location.pathname !== prevProps.location.pathname) {
+      if (this.props.location.pathname === '/') {
+        this.wrapper.animate([{ height: '20vh' }, { height: '70vh' }], {
+          duration: 300,
+          fill: 'forwards',
+          easing: 'cubic-bezier(0.86, 0, 0.07, 1)',
+          iterations: 1,
+        });
+      } else {
+        this.wrapper.animate([{ height: '70vh' }, { height: '20vh' }], {
+          duration: 300,
+          fill: 'forwards',
+          easing: 'cubic-bezier(0.86, 0, 0.07, 1)',
+          iterations: 1,
+        });
+      }
+    }
+  };
+
+  render() {
+    const { data, location } = this.props;
+    return (
+      <HeaderWrapper
+        isHome={location.pathname === '/'}
+        ref={wrapper => (this.wrapper = ReactDOM.findDOMNode(wrapper))}
+      >
+        <HeaderContainer>
+          <h1 style={{ margin: 0 }}>
+            <Link
+              to="/"
+              style={{
+                color: 'white',
+                textDecoration: 'none',
+              }}
+            >
+              Gatsby
+            </Link>
+          </h1>
+          <MainNav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="about">About</Link>
+              </li>
+            </ul>
+          </MainNav>
+        </HeaderContainer>
+        <Img
+          sizes={data.background.sizes}
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            opacity: 0.3,
+          }}
+        />
+      </HeaderWrapper>
+    );
+  }
+}
---
+++ src/pages/about.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import picture from '../images/iceland2.jpg';
+const About = () => {
+  return (
+    <div>
+      <h1>About us</h1>
+      <img src={picture} alt="icelandic beach" />
+      <p>
+        Lorem ipsum dolor amet irony raw denim hashtag, hot chicken fingerstache
+        beard chillwave microdosing readymade fam. Edison bulb listicle
+        skateboard lyft. Narwhal sustainable shoreditch, poke pour-over vinyl
+        bicycle rights XOXO keffiyeh seitan vice cronut lyft. Narwhal pour-over
+        thundercats pinterest leggings cold-pressed shabby chic celiac retro pug
+        green juice. Keffiyeh pinterest coloring book, shabby chic literally
+        shaman green juice franzen lumbersexual fashion axe wayfarers migas pok
+        pok. Ennui 3 wolf moon schlitz lo-fi health goth selvage. Enamel pin pok
+        pok kale chips trust fund, hexagon 8-bit single-origin coffee. Vape
+        celiac bitters, cliche hot chicken snackwave tumblr flannel church-key
+        sustainable gentrify pinterest wolf wayfarers art party.
+      </p>
+    </div>
+  );
+};
+
+export default About;
